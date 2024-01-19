05a26838ba832d2de279ebb88a0d18ed561e9637
wo4mei3 <wo4mei3@gmail.com>
2024-01-19 11:24:33 +0900

    Add type_conv in typing.ml

--- lib/typing.ml
+++ lib/typing.ml
@@ -2,7 +2,7 @@ type expr =
   | EConst of expr Syntax.ty * Syntax.value
   | EVar of expr Syntax.ty * Syntax.id
   | EBinary of expr Syntax.ty * Syntax.binary * expr * expr
-  | EAssign of expr Syntax.ty * expr * expr
+  | EAssign of expr Syntax.ty * Syntax.binary * expr * expr
   | EUnary of expr Syntax.ty * Syntax.unary * expr
   | ESizeof of expr Syntax.ty * expr Syntax.ty
   | EPostfix of expr Syntax.ty * expr * expr Syntax.postfix
@@ -10,3 +10,34 @@ type expr =
   | ECast of expr Syntax.ty * expr
   | ECompoundLit of expr Syntax.ty * expr Syntax.init
 [@@deriving show]
+
+let rec type_conv =
+  let open Syntax in
+  function
+  | TFun (ty, decl) ->
+      TPtr
+        (TFun (type_conv ty, List.map (fun (n, ty) -> (n, type_conv ty)) decl))
+  | TArr (ty, _) -> TConstPtr (type_conv ty)
+  | TConstPtr ty -> TConstPtr (type_conv ty)
+  | TPtr ty -> TPtr (type_conv ty)
+  | TDeclSpec l -> (
+      if
+        List.exists
+          (function
+            | TsInt | TsShort | TsLong | TsChar | TsSigned | TsUnsigned
+            | TsFloat | TsDouble ->
+                true
+            | _ -> false)
+          l
+      then TDeclSpec [ TsInt ]
+      else
+        try
+          let e =
+            List.find
+              (function
+                | TsVoid | TsStruct _ | TsUnion _ | TsTypedef _ -> true
+                | _ -> false)
+              l
+          in
+          TDeclSpec [ e ]
+        with _ -> failwith "type_conv")
