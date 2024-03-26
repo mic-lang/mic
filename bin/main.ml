dcd22dc16b19f95df0326f3e2590f1045edff13d
wo4mei3 <wo4mei3@gmail.com>
2024-03-26 16:36:14 +0900

    change several files

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,22 @@
-let out_file fname =
+let tmp_file1 fname =
   let fnlen = String.length fname in
   let suffix = if 3 < fnlen then String.sub fname (fnlen - 2) 2 else "" in
-  if suffix = ".c" then Printf.sprintf "%s.i" (String.sub fname 0 (fnlen - 2))
+  if suffix = ".c" then Printf.sprintf "%s.c" (String.sub fname 0 (fnlen - 2))
   else if suffix = "mi" then
     Printf.sprintf "%s.c" (String.sub fname 0 (fnlen - 3))
   else Printf.sprintf "%s.c" fname
+
+let tmp_file2 fname =
+  let fnlen = String.length fname in
+  let suffix = if 3 < fnlen then String.sub fname (fnlen - 2) 2 else "" in
+  if suffix = ".c" then Printf.sprintf "%s.i" (String.sub fname 0 (fnlen - 2))
+  else Printf.sprintf "%s.i" fname
+
+let out_file fname =
+  let fnlen = String.length fname in
+  let suffix = if 2 < fnlen then String.sub fname (fnlen - 2) 2 else "" in
+  if suffix = ".c" then Printf.sprintf "%s.o" (String.sub fname 0 (fnlen - 2))
+  else Printf.sprintf "%s.o" fname
 
 let show_pos fname filebuf =
   let pos = filebuf.Lexing.lex_start_p in
@@ -26,11 +38,10 @@ let () =
     exit (-1))
   else
     let fname = Sys.argv.(1) in
-    ignore (Sys.command ("cp " ^ fname ^ " " ^ out_file fname));
-    let fname = out_file fname in
-    print_endline fname;
-    ignore (Sys.command ("gcc -E " ^ fname ^ " > " ^ out_file fname));
-    let inchan = open_in (out_file fname) in
+    ignore (Sys.command ("cp " ^ fname ^ " " ^ tmp_file1 fname));
+    let tmp = tmp_file1 fname in
+    ignore (Sys.command ("gcc -E " ^ tmp ^ " > " ^ tmp_file2 tmp));
+    let inchan = open_in (tmp_file2 tmp) in
     let filebuf = Lexing.from_channel inchan in
     try
       ignore (Mic.Parser.translation_unit Mic.Lexer.token filebuf);
@@ -43,10 +54,13 @@ let () =
               (fun i x -> (i, x))
               (Mic.Typing.type_program (List.rev !Mic.Env.program))));
       let program = Mic.Cgen.gen_program (List.rev !Mic.Env.program) in
-      let outchan = open_out fname in
-      Printf.fprintf outchan "%s" program
+      let outchan = open_out tmp in
+      Printf.fprintf outchan "%s" program;
+      let tmp = tmp_file1 tmp in
+      print_endline (out_file tmp);
+      ignore (Sys.command ("gcc " ^ tmp ^ " -o " ^ out_file tmp ^ " -lmimalloc"));
     with
     | Failure msg -> show_error "%s" msg
     | _ ->
-        show_pos (out_file fname) filebuf;
+        show_pos fname filebuf;
         show_error "parser: syntax error near '%s'" (Lexing.lexeme filebuf)
